e5fbb8de524a91aae31e0bf0066b2e77db8acfb7
IvaSv <148450331+IvaSv@users.noreply.github.com>
2025-08-26 16:21:54 +0200

    Security CORS update

--- backend/src/main/java/backend/security/WebSecurity.java
+++ backend/src/main/java/backend/security/WebSecurity.java
@@ -16,6 +16,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableMethodSecurity(prePostEnabled = true) //this enables method-level security: use @Secured to secure individual methods
 @Configuration
@@ -75,6 +80,10 @@ public class WebSecurity  {
         );
 
         http.csrf(csrf -> csrf.disable());
+
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
 
@@ -98,4 +107,19 @@ public class WebSecurity  {
         return new JWTAuthenticationFilter();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                "https://zupna-stranica-sibenik.onrender.com", // frontend Render
+                "http://localhost:5173"                       // lokalno Vite
+        ));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
