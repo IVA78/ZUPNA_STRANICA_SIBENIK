47e6d91955e6be7f735e8a3c8b5fc08cb0e1e75d
IvaSv <148450331+IvaSv@users.noreply.github.com>
2025-08-26 15:41:41 +0200

    Update vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     proxy: {
       "/api": "https://zupna-stranica-sibenik-b.onrender.com", // Proxy preusmjerava "/api" na backend
