5a163b05dc2f17963532bd1ff2ba65e043c2efc4
IvaSv <148450331+IvaSv@users.noreply.github.com>
2025-09-06 18:34:19 +0200

    routes update

--- frontend/server.cjs
+++ frontend/server.cjs
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3000;
 // Serviraj statične fajlove
 app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Frontend running on port ${port}`);
 });
