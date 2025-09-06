3214a4069fa604235c8a406e855beb821000c4c7
IvaSv <148450331+IvaSv@users.noreply.github.com>
2025-09-06 18:36:49 +0200

    hvatanje nestatickih ruta

--- frontend/server.cjs
+++ frontend/server.cjs
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 // Serviraj statične fajlove
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
